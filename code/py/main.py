651496560ddd63703f22af1551f722b0c610cc52
jay <jadav7740@gmail.com>
2023-07-05 10:33:43 +0530

    #1 got the basic script running with proper libs & imports

---
+++ code/py/main.py
@@ -0,0 +1,14 @@
+import test_p1
+
+print ("Always executed")
+
+if __name__ == "__main__":
+	print ("Executed when invoked directly")
+    #p1 = TestP1()
+    #p1.setup_method("")
+    #p1.test_p1()
+    #p1.teardown_method("")
+
+#else:
+	#print ("Executed when imported")
+
