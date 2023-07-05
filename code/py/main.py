a59dbb870606dbc7da41d6ad952648f578f64082
jay <jadav7740@gmail.com>
2023-07-05 13:11:59 +0530

    #1 added the chrome webdriver instructions

--- code/py/main.py
+++ code/py/main.py
@@ -1,14 +1,13 @@
-import test_p1
+from test_p1 import TestP1
 
-print ("Always executed")
+def main():
+    print("Entered the main function")
+    p1 = TestP1()
+    p1.setup_method("")
+    p1.test_p1()
+    p1.teardown_method("")
 
 if __name__ == "__main__":
-	print ("Executed when invoked directly")
-    #p1 = TestP1()
-    #p1.setup_method("")
-    #p1.test_p1()
-    #p1.teardown_method("")
-
-#else:
-	#print ("Executed when imported")
+    print ("Executed when invoked directly")
+    main()
 
